c0c51920378a70e3dac8fe29eccb8f4e3b54647e
aRkideR <111351961+aRkideR@users.noreply.github.com>
2020-10-27 20:07:32 +0000

    Version updated.

--- A2C.py
+++ A2C.py
@@ -54,3 +54,84 @@ class A2C(Agent):
             self.actor_optimizer = RMSprop(self.actor.parameters(), lr=self.actor_lr)
             self.critic_optimizer = RMSprop(self.critic.parameters(), lr=self.critic_lr)
         if self.use_cuda:
+            self.actor.cuda()
+
+    # agent interact with the environment to collect experience
+    def interact(self):
+        super(A2C, self)._take_n_steps()
+
+    # train on a roll out batch
+    def train(self):
+        if self.n_episodes <= self.episodes_before_train:
+            pass
+
+        batch = self.memory.sample(self.batch_size)
+        states_var = to_tensor_var(batch.states, self.use_cuda).view(-1, self.state_dim)
+        one_hot_actions = index_to_one_hot(batch.actions, self.action_dim)
+        actions_var = to_tensor_var(one_hot_actions, self.use_cuda).view(-1, self.action_dim)
+        rewards_var = to_tensor_var(batch.rewards, self.use_cuda).view(-1, 1)
+
+        # update actor network
+        self.actor_optimizer.zero_grad()
+        action_log_probs = self.actor(states_var)
+        entropy_loss = th.mean(entropy(th.exp(action_log_probs)))
+        action_log_probs = th.sum(action_log_probs * actions_var, 1)
+        values = self.critic(states_var, actions_var)
+        advantages = rewards_var - values.detach()
+        pg_loss = -th.mean(action_log_probs * advantages)
+        actor_loss = pg_loss - entropy_loss * self.entropy_reg
+        actor_loss.backward()
+        if self.max_grad_norm is not None:
+            nn.utils.clip_grad_norm(self.actor.parameters(), self.max_grad_norm)
+        self.actor_optimizer.step()
+
+        # update critic network
+        self.critic_optimizer.zero_grad()
+        target_values = rewards_var
+        if self.critic_loss == "huber":
+            critic_loss = nn.functional.smooth_l1_loss(values, target_values)
+        else:
+            critic_loss = nn.MSELoss()(values, target_values)
+        critic_loss.backward()
+        if self.max_grad_norm is not None:
+            nn.utils.clip_grad_norm(self.critic.parameters(), self.max_grad_norm)
+        self.critic_optimizer.step()
+
+    # predict softmax action based on state
+    def _softmax_action(self, state):
+        state_var = to_tensor_var([state], self.use_cuda)
+        softmax_action_var = th.exp(self.actor(state_var))
+        if self.use_cuda:
+            softmax_action = softmax_action_var.data.cpu().numpy()[0]
+        else:
+            softmax_action = softmax_action_var.data.numpy()[0]
+        return softmax_action
+
+    # choose an action based on state with random noise added for exploration in training
+    def exploration_action(self, state):
+        softmax_action = self._softmax_action(state)
+        epsilon = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * \
+                                  np.exp(-1. * self.n_steps / self.epsilon_decay)
+        if np.random.rand() < epsilon:
+            action = np.random.choice(self.action_dim)
+        else:
+            action = np.argmax(softmax_action)
+        return action
+
+    # choose an action based on state for execution
+    def action(self, state):
+        softmax_action = self._softmax_action(state)
+        action = np.argmax(softmax_action)
+        return action
+
+    # evaluate value for a state-action pair
+    def value(self, state, action):
+        state_var = to_tensor_var([state], self.use_cuda)
+        action = index_to_one_hot(action, self.action_dim)
+        action_var = to_tensor_var([action], self.use_cuda)
+        value_var = self.critic(state_var, action_var)
+        if self.use_cuda:
+            value = value_var.data.cpu().numpy()[0]
+        else:
+            value = value_var.data.numpy()[0]
+        return value
