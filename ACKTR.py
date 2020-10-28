95897c492e2c5c054ec133ba1befa941f4097cb9
aRkideR <111351961+aRkideR@users.noreply.github.com>
2020-10-28 12:08:47 +0000

    Fix some formatting issues

--- ACKTR.py
+++ ACKTR.py
@@ -7,4 +7,8 @@ import numpy as np
 from A2C import A2C
 from common.Model import ActorCriticNetwork
 from common.kfac import KFACOptimizer
-from common.utils import index_to_one_ho
+from common.utils import index_to_one_hot, entropy, to_tensor_var
+
+
+class DisjointACKTR(A2C):
+    
