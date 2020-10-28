f5a4e4b90277b3c59102cdfb39eef56716730358
aRkideR <111351961+aRkideR@users.noreply.github.com>
2020-10-28 12:40:01 +0000

    Initial code

--- ACKTR.py
+++ ACKTR.py
@@ -13,4 +13,6 @@ from common.utils import index_to_one_hot, entropy, to_tensor_var
 class DisjointACKTR(A2C):
     """
     An agent learned with ACKTR
-    Using disjoint actor and cri
+    Using disjoint actor and critic results in instability in training.
+    """
+    def __init__(self, env, state_dim
