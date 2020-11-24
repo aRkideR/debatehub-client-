78122f3aa2a151b8a12ad8696b5158847d094fc8
aRkideR <111351961+aRkideR@users.noreply.github.com>
2020-11-24 06:43:34 +0000

    re-run make idl.

--- ACKTR.py
+++ ACKTR.py
@@ -49,4 +49,5 @@ class DisjointACKTR(A2C):
         batch = self.memory.sample(self.batch_size)
         states_var = to_tensor_var(batch.states, self.use_cuda).view(-1, self.state_dim)
         one_hot_actions = index_to_one_hot(batch.actions, self.action_dim)
-        actions_var = to_tensor_var(one_ho
+        actions_var = to_tensor_var(one_hot_actions, self.use_cuda).view(-1, self.action_dim)
+        rewards_var = to_t
