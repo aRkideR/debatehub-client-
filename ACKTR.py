f2076912f26d5aad2f47c508426d0516f38dda3b
aRkideR <111351961+aRkideR@users.noreply.github.com>
2020-12-07 09:16:57 +0000

    cleaning code

--- ACKTR.py
+++ ACKTR.py
@@ -84,4 +84,5 @@ class DisjointACKTR(A2C):
             sample_values = (values + values_noise.view(-1, 1)).detach()
             if self.critic_loss == "huber":
                 vf_fisher_loss = - self.vf_fisher_coef * nn.functional.smooth_l1_loss(values, sample_values)
-        
+            else:
+                vf_fisher_loss = - self.vf_fisher_coef *
