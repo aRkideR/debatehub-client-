633d495265a8981d1c4a8db082c20797ccdc5917
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-01-07 08:44:10 +0000

    dependency fix

--- ACKTR.py
+++ ACKTR.py
@@ -122,4 +122,6 @@ class JointACKTR(A2C):
                  actor_hidden_size, critic_hidden_size,
                  actor_output_act, critic_loss,
                  actor_lr, critic_lr,
-                 optimizer_type, entropy_re
+                 optimizer_type, entropy_reg,
+                 max_grad_norm, batch_size, episodes_before_train,
+    
