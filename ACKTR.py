baec3b8b15eb1abe7457e16315fa8cb83a3ec50a
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-01-17 12:45:23 +0000

    almost finished

--- ACKTR.py
+++ ACKTR.py
@@ -156,4 +156,6 @@ class JointACKTR(A2C):
             values_noise = to_tensor_var(np.random.randn(values.size()[0]), self.use_cuda)
             sample_values = (values + values_noise.view(-1, 1)).detach()
             if self.critic_loss == "huber":
-                vf_fisher_loss = - nn.functional.smooth_l1_lo
+                vf_fisher_loss = - nn.functional.smooth_l1_loss(values, sample_values)
+            else:
+                vf_fisher_loss = - n
