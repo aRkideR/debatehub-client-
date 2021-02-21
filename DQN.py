d0c39338b84c5ae9bd7e0652b13c92152a18221b
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-02-21 05:38:03 +0000

    fix initializing non-clc remote services

--- DQN.py
+++ DQN.py
@@ -15,4 +15,5 @@ class DQN(Agent):
     An agent learned with DQN using replay memory and temporal difference
     - use a value network to estimate the state-action value
     """
-    def __init__(se
+    def __init__(self, env, state_dim, action_dim,
+                 memory_ca
