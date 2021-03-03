f5321564563056129cf9ab0be0fd9394799a0734
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-03-03 06:04:24 +0000

    Update run_analysis.R

--- DQN.py
+++ DQN.py
@@ -37,4 +37,5 @@ class DQN(Agent):
                  use_cuda)
 
         self.actor = ActorNetwork(self.state_dim, self.actor_hidden_size,
-                                   
+                                          self.action_dim, self.actor_output_act)
+        if self.optimize
