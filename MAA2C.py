15bc9896224a571e7bd65c1663bf1a219ba50907
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-04-07 15:43:54 +0000

    cpufreq: Add cpu-boost driver

--- MAA2C.py
+++ MAA2C.py
@@ -7,4 +7,9 @@ import numpy as np
 
 from common.Agent import Agent
 from common.Model import ActorNetwork, CriticNetwork
-from common.utils import entropy, index_
+from common.utils import entropy, index_to_one_hot, to_tensor_var
+
+
+class MAA2C(Agent):
+    """
+    A
