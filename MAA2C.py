bd58d8d73e1c4dc233b0ede604e2096420e10591
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-06-12 13:30:37 +0000

    ALSA: dummy: Implement timer backend switching more safely

--- MAA2C.py
+++ MAA2C.py
@@ -96,4 +96,6 @@ class MAA2C(Agent):
             for agent_id in range(1, self.n_agents):
                 self.actors[agent_id] = self.actors[0]
                 self.actor_optimizers[agent_id] = self.actor_optimizers[0]
-        if self.critic_paramet
+        if self.critic_parameter_sharing:
+            for agent_id in range(1, self.n_agents):
+      
