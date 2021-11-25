d21f9095989f7285c11ac51059bb816435a18e02
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-11-25 21:50:18 +0000

    Minor code cleanup.

--- MADDPG.py
+++ MADDPG.py
@@ -11,3 +11,15 @@ from common.Memory import ReplayMemory
 from common.Model import ActorNetwork, CriticNetwork
 from common.utils import to_tensor_var
 
+
+class MADDPG(object):
+    """
+    An multi-agent learned with Deep Deterministic Policy Gradient using Actor-Critic framework
+    - Actor takes state as input
+    - Critic takes both state and action as input
+    """
+    def __init__(self):
+        """
+        TODO
+        """
+        pass
