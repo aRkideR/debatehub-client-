1635ef79dd8c94dea82d15232f1c14333b1a9f33
aRkideR <111351961+aRkideR@users.noreply.github.com>
2021-11-30 13:34:06 +0000

    doxy

--- MADQN.py
+++ MADQN.py
@@ -5,4 +5,6 @@ from torch.optim import Adam, RMSprop
 
 import numpy as np
 
-from common.Memory import ReplayMemo
+from common.Memory import ReplayMemory
+from common.Model import ActorNetwork
+from common.utils import to_tensor_va
