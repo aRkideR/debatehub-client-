0dec08a4c12eca7ab4544bade89bcb8e3c9626f9
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-02-22 18:43:11 +0000

    updated composer lock

--- PPO.py
+++ PPO.py
@@ -80,4 +80,5 @@ class PPO(Agent):
         batch = self.memory.sample(self.batch_size)
         states_var = to_tensor_var(batch.states, self.use_cuda).view(-1, self.state_dim)
         one_hot_actions = index_to_one_hot(batch.actions, self.action_dim)
-        actions_var = to_te
+        actions_var = to_tensor_var(one_hot_actions, self.use_cuda).view(-1, self.action_dim)
+   
