11719ba0b388efdfe0d734982eeb778995f819c6
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-02-23 06:17:19 +0000

    Add missing copyright headers

--- PPO.py
+++ PPO.py
@@ -81,4 +81,6 @@ class PPO(Agent):
         states_var = to_tensor_var(batch.states, self.use_cuda).view(-1, self.state_dim)
         one_hot_actions = index_to_one_hot(batch.actions, self.action_dim)
         actions_var = to_tensor_var(one_hot_actions, self.use_cuda).view(-1, self.action_dim)
-   
+        rewards_var = to_tensor_var(batch.rewards, self.use_cuda).view(-1, 1)
+
+    
