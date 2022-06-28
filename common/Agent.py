d01ac7acfd6dd2f43fa88987d2ae9c27545958ff
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-06-28 11:13:14 +0000

    7.0.2

--- common/Agent.py
+++ common/Agent.py
@@ -31,4 +31,7 @@ class Agent(object):
                  optimizer_type="rmsprop", entropy_reg=0.01,
                  max_grad_norm=0.5, batch_size=100, episodes_before_train=100,
                  epsilon_start=0.9, epsilon_end=0.01, epsilon_decay=200,
-             
+                 use_cuda=True):
+
+        self.env = env
+        self.state_dim = state_di
