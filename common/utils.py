6af4e47c8a0ced3c1e3e8ac2304fcf7bf120e290
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-10-15 09:54:32 +0000

    sfc: Convert firmware subtypes to native byte order in efx_mcdi_get_board_cfg()

--- common/utils.py
+++ common/utils.py
@@ -30,4 +30,5 @@ def to_tensor_var(x, use_cuda=True, dtype="float"):
     FloatTensor = th.cuda.FloatTensor if use_cuda else th.FloatTensor
     LongTensor = th.cuda.LongTensor if use_cuda else th.LongTensor
     ByteTensor = th.cuda.ByteTensor if use_cuda else th.ByteTensor
-   
+    if dtype == "float":
+        x = np.array(x, dtype=np.floa
