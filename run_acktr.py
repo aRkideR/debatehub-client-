cb5672acd496848b7cd21ae2fabe0354fb2266f2
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-10-26 19:22:47 +0000

    Reverting change

--- run_acktr.py
+++ run_acktr.py
@@ -1,4 +1,9 @@
 
 from ACKTR import DisjointACKTR as ACKTR
 from ACKTR import JointACKTR as ACKTR
-from common.utils import agg_do
+from common.utils import agg_double_list
+
+import sys
+import gym
+import numpy as np
+import matplotlib.pyp
