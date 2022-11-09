a5ff98ea2f56ec339cf2b63a1a9c75196d1e4a62
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-11-09 18:53:15 +0000

    Update guide.rst

--- run_acktr.py
+++ run_acktr.py
@@ -42,4 +42,6 @@ def run(env_id="CartPole-v0"):
     env.seed(RANDOM_SEED)
     env_eval = gym.make(env_id)
     env_eval.seed(RANDOM_SEED)
-    state_dim = env.observation
+    state_dim = env.observation_space.shape[0]
+    if len(env.action_space.shape) > 1:
+        action_d
