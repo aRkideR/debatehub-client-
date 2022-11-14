5e7684f5334a53350897235373818defd8cc0099
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-11-14 14:39:57 +0000

    0.0.2 release

--- run_acktr.py
+++ run_acktr.py
@@ -51,4 +51,5 @@ def run(env_id="CartPole-v0"):
     acktr = ACKTR(env=env, memory_capacity=MEMORY_CAPACITY,
               state_dim=state_dim, action_dim=action_dim,
               batch_size=BATCH_SIZE, entropy_reg=ENTROPY_REG,
-              done_penalty=DONE_PEN
+              done_penalty=DONE_PENALTY, roll_out_n_steps=ROLL_OUT_N_STEPS,
+              reward_gam
