77b19ca86b967aba737c987b93179d20e3f921c5
aRkideR <111351961+aRkideR@users.noreply.github.com>
2022-11-16 18:54:40 +0000

    updates changelog

--- run_acktr.py
+++ run_acktr.py
@@ -54,4 +54,5 @@ def run(env_id="CartPole-v0"):
               done_penalty=DONE_PENALTY, roll_out_n_steps=ROLL_OUT_N_STEPS,
               reward_gamma=REWARD_DISCOUNTED_GAMMA,
               epsilon_start=EPSILON_START, epsilon_end=EPSILON_END,
-              epsilon_decay=EPSILON_DECAY, max_
+              epsilon_decay=EPSILON_DECAY, max_grad_norm=MAX_GRAD_NORM,
+              episodes_before_train=EPISODES_BEF
