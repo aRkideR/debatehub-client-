3ec539c7183c807bc613800670c89c716ac0c56e
aRkideR <111351961+aRkideR@users.noreply.github.com>
2023-01-16 17:19:50 +0000

    ld/

--- run_ppo.py
+++ run_ppo.py
@@ -46,4 +46,7 @@ def run(env_id="CartPole-v0"):
     env_eval.seed(RANDOM_SEED)
     state_dim = env.observation_space.shape[0]
     if len(env.action_space.shape) > 1:
-        action_dim = env.
+        action_dim = env.action_space.shape[0]
+    else:
+        action_dim = env.action_space.n
+
