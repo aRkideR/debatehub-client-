31f0a2e6b199fdd921f62699195c4d6bdb7b1da2
aRkideR <111351961+aRkideR@users.noreply.github.com>
2023-01-19 17:45:21 +0000

    [zendframework/zf2#1199] Allow testing barcode

--- run_ppo.py
+++ run_ppo.py
@@ -52,4 +52,5 @@ def run(env_id="CartPole-v0"):
 
     ppo = PPO(env=env, memory_capacity=MEMORY_CAPACITY,
               state_dim=state_dim, action_dim=action_dim,
-              batch_si
+              batch_size=BATCH_SIZE, entropy_reg=ENTROPY_REG,
+              done_penalty=DONE
